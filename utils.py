a1e10a5d2f9743cdc1a4e4750434deb037b0bd44
Mark Oulitin <fragoric@gmail.com>
2021-07-12 18:13:46 +0300

    Added Doc for utils.py and more results

--- utils.py
+++ utils.py
@@ -7,6 +7,11 @@ import pandas as pd
 
 def save_to_dict(dict, iteration, hp_values, TPR, FPR, ACC, PRECISION, AUC_ROC, AUC_Precision_Recall, train_time,
                  inference_time):
+    """"
+    append results to appropriate column in the dictionary
+    The dictionary which will be converted to csv file in save_to_csv function
+    The key of dictionary is the column name and the value is list of results of the performance of that metric
+    """
     dict['Hyper-Parameters Values'].append(hp_values)
     dict['Cross Validation [1-10]'].append(iteration)
     dict['TPR'].append(np.round(TPR, 3))
@@ -20,9 +25,14 @@ def save_to_dict(dict, iteration, hp_values, TPR, FPR, ACC, PRECISION, AUC_ROC, 
 
 
 def create_dict(dataset_name, algorithm_name):
+    """
+    Initialize the dict which will be converted to csv file
+    dataset name and algorithm name will be the same for all rows
+    """
     output = dict()
     output['Dataset Name'] = dataset_name
     output['Algorithm Name'] = algorithm_name
+    # number of the iteration of the Outer cross validation
     output['Cross Validation [1-10]'] = []
     output['Hyper-Parameters Values'] = []
     output['TPR'] = []
@@ -40,6 +50,10 @@ results_dir = 'Results'
 
 
 def save_to_csv(dict, filename):
+    """"
+    Save dict to csv file, if the file already exits then append dict values as rows to the file
+    """
+
     global results_dir
     df = pd.DataFrame.from_dict(dict)
     file_path = results_dir + '/' + filename + '.csv'
@@ -51,12 +65,21 @@ def save_to_csv(dict, filename):
 
 
 def setup():
+    """
+    Create directory ./Results
+    """
     global results_dir
     if not os.path.exists(results_dir):
         os.mkdir(results_dir)
 
 
 def merge_results(result_filename, results_dir):
+    """
+    data of each csv file in ./Results directory will be concatenated into a dataframe
+    :param result_filename:
+    :param results_dir:
+    :return: dataframe
+    """
     list_of_results = []
     for filename in os.listdir(results_dir):
         list_of_results.append(pd.read_csv(results_dir + "/" + filename))

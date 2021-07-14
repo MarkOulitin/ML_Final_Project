46188e877be3df4bd73cc0d47e921db4fe4a61f5
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2021-07-14 11:54:05 +0300

    Statistics tests bug fixes and further implementation

--- statistics_tests.py
+++ statistics_tests.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import scikit_posthocs
 import scipy.stats
+import utils
 
 CONFIDENCE_ALPHA = 0.05
 
 
-def statistic_test(data_filename, metric, alpha):
+def statistic_test(df, metric, alpha):
     """
     Performs friedman statistic test with confidence :alpha:
     on the performance the specified :metric: of the algorithms on the datasets.
@@ -17,19 +18,30 @@ def statistic_test(data_filename, metric, alpha):
     Uses the mean of the specified metric of each algorithm on the datasets as
     the measurement for the friedman test.
 
-    :param data_filename: The file name to read the algorithms performances on
+    :param df: The data frame from which to read the algorithms performances on
     the various datasets
     :param metric: The metric to use for the statistic test
     :param alpha: The confidence value used to determine whether the test is
     significant
     :return: The results of the post-hoc test if significant, otherwise None.
     """
-    measurements = split_df_to_measurements(pd.read_excel(data_filename), metric)
+    algorithms_measurements_pairs_list = split_df_to_measurements(df, metric)
+    measurements = list(map(lambda p: p[1], algorithms_measurements_pairs_list))
     statistic, pvalue = scipy.stats.friedmanchisquare(*measurements)
+    posthoc_results = None
     if pvalue < alpha:
-        return scikit_posthocs.posthoc_nemenyi_friedman(measurements_to_posthoc(measurements))
-    else:
-        return None
+        posthoc_measurements = measurements_to_posthoc(measurements)
+        posthoc_results = scikit_posthocs.posthoc_nemenyi_friedman(posthoc_measurements)
+        posthoc_results = posthoc_results.rename(
+            index=algorithm_indices_to_dict(algorithms_measurements_pairs_list, posthoc_results.index),
+            columns=algorithm_indices_to_dict(algorithms_measurements_pairs_list, posthoc_results.columns)
+        )
+
+    return pvalue, posthoc_results
+
+
+def algorithm_indices_to_dict(algorithms_measurements_pairs_list, indices):
+    return dict(map(lambda p: (p, algorithms_measurements_pairs_list[p][0]), indices))
 
 
 def measurements_to_posthoc(measurements):
@@ -40,7 +52,7 @@ def measurements_to_posthoc(measurements):
     :return: The converted measurements
     """
     # code adopted from https://www.statology.org/nemenyi-test-python/
-    a = np.array(*measurements)
+    a = np.array(measurements)
     a = np.transpose(a)
     return a
 
@@ -72,8 +84,27 @@ def split_df_to_measurements(df, metric):
 
     # split to the lists of the means per algorithm
     df = df[metric_col_name].apply(list)
-    return df.values
+    algorithms_measurements_pairs_list = list(df.to_dict().items())
+    return algorithms_measurements_pairs_list
+
+
+def run_statistic_test_from_results(results_file_name, metric, alpha, results_dir, posthoc_results_file_name):
+    utils.merge_results(results_file_name, results_dir=results_dir)
+    df = pd.read_excel(results_file_name)
+    pvalue, posthoc_results = statistic_test(df, metric, alpha)
+    if posthoc_results is None:
+        print(f'Results are not significant alpha={alpha}, pvalue={pvalue}')
+    else:
+        print(f'Results are significant alpha={alpha}, pvalue={pvalue}')
+        print(posthoc_results)
+        posthoc_results.to_excel(posthoc_results_file_name)
 
 
 if __name__ == '__main__':
-    statistic_test('Results.xlsx', 'Accuracy', 0.05)
+    run_statistic_test_from_results(
+        'Results.xlsx',
+        metric='TPR',
+        alpha=0.05,
+        results_dir='Final_Results',
+        posthoc_results_file_name='Posthoc_Results.xlsx'
+    )
